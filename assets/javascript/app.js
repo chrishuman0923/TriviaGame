6991f8a3872239c6b6479ebcf86ab4d0f0dd21a3
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-10-27 09:16:17 -0400

    Add document.ready function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
-var $mainContent = $(".mainContent"),
+//wait till page is ready
+$(document).ready(function () {
+    var $mainContent = $(".mainContent"),
     secondsRemaining,
     $timer = $("#timer"),
     $clock = $("#clock"),
@@ -50,218 +52,219 @@ var $mainContent = $(".mainContent"),
     incorrectElem,
     scoreElem;
 
-//Helper function to convert seconds to milliseconds for intervals
-function secToMs(seconds) {
-    return seconds * 1000;
-}
+    //Helper function to convert seconds to milliseconds for intervals
+    function secToMs(seconds) {
+        return seconds * 1000;
+    }
 
-function resetTimer() {
-    //Reset timer to original value
-    secondsRemaining = 10;
+    function resetTimer() {
+        //Reset timer to original value
+        secondsRemaining = 10;
 
-    //Update DOM
-    $timer.text(secondsRemaining);
-}
-
-function reduceTimer() {
-    //Reduce seconds by 1
-    secondsRemaining -= 1;
-
-    //Stop interval when the time has hit 0
-    if (secondsRemaining <= 0) {
-        disableBackgroundProcesses();
-
-        //Is there another question
-        if (questionNum < questions.length-1) {
-            timesUp();
-        } else {
-            gameOver();
-        }
-    } else {
         //Update DOM
         $timer.text(secondsRemaining);
     }
-}
 
-function gameOver() {
-    var incorrectNum = totalNum - correctNum,
-        score = ((correctNum / totalNum) * 100).toFixed(2) + "%";
+    function reduceTimer() {
+        //Reduce seconds by 1
+        secondsRemaining -= 1;
 
-    correctElem = $("<p>").text('Number Correct: ' + correctNum).addClass("correctAnswer");
-    incorrectElem = $("<p>").text('Number Incorrect: ' + incorrectNum).addClass("incorrectAnswer");
-    scoreElem = $("<p>").text('Score: ' + score);
-    
-    //Update game area by hiding and updating relevant objects
-    $clock.hide();
-    $triviaArea.html([correctElem, incorrectElem, scoreElem]);
+        //Stop interval when the time has hit 0
+        if (secondsRemaining <= 0) {
+            disableBackgroundProcesses();
 
-    $restartBtn.show();
-}
-
-function createQuestion() {
-    var question = $("<h2>").text(questions[questionNum].question),
-        form = $("<form>").addClass("answerChoices");
-
-    //Append elements to DOM
-    $triviaArea.append([question, form]);
-    
-    //Loop through, create and append answer choices onto DOM
-    for (var i = 0; i < questions[questionNum].choices.length; i++) {
-        var choice = $("<input>").attr({
-                "type": "radio",
-                "name": "option",
-                "class": "choice"
-            }),
-            label = $("<label>").attr("data-choice", [i]);
-        
-        //Adds attribute to identify which choice is correct and which are wrong
-        if (i.toString() === questions[questionNum].correct) {
-            label.attr("data-answer", "true");
+            //Is there another question
+            if (questionNum < questions.length-1) {
+                timesUp();
+            } else {
+                gameOver();
+            }
         } else {
-            label.attr("data-answer", "false");
+            //Update DOM
+            $timer.text(secondsRemaining);
+        }
+    }
+
+    function gameOver() {
+        var incorrectNum = totalNum - correctNum,
+            score = ((correctNum / totalNum) * 100).toFixed(2) + "%";
+
+        correctElem = $("<p>").text('Number Correct: ' + correctNum).addClass("correctAnswer");
+        incorrectElem = $("<p>").text('Number Incorrect: ' + incorrectNum).addClass("incorrectAnswer");
+        scoreElem = $("<p>").text('Score: ' + score);
+        
+        //Update game area by hiding and updating relevant objects
+        $clock.hide();
+        $triviaArea.html([correctElem, incorrectElem, scoreElem]);
+
+        $restartBtn.show();
+    }
+
+    function createQuestion() {
+        var question = $("<h2>").text(questions[questionNum].question),
+            form = $("<form>").addClass("answerChoices");
+
+        //Append elements to DOM
+        $triviaArea.append([question, form]);
+        
+        //Loop through, create and append answer choices onto DOM
+        for (var i = 0; i < questions[questionNum].choices.length; i++) {
+            var choice = $("<input>").attr({
+                    "type": "radio",
+                    "name": "option",
+                    "class": "choice"
+                }),
+                label = $("<label>").attr("data-choice", [i]);
+            
+            //Adds attribute to identify which choice is correct and which are wrong
+            if (i.toString() === questions[questionNum].correct) {
+                label.attr("data-answer", "true");
+            } else {
+                label.attr("data-answer", "false");
+            }
+
+            //Appends the label to the DOM
+            $(".answerChoices").append(label);
+
+            //Appends choice within the label that was created
+            $('[data-choice=' + [i] + ']').append([choice, questions[questionNum].choices[i]], "<br/>");
         }
 
-        //Appends the label to the DOM
-        $(".answerChoices").append(label);
-
-        //Appends choice within the label that was created
-        $('[data-choice=' + [i] + ']').append([choice, questions[questionNum].choices[i]], "<br/>");
+        resetTimer();
     }
 
-    resetTimer();
-}
+    function nextQuestion() {
+        //if timer is hidden, show it
+        if ($clock.not(':visible')) {
+            $clock.show();
+        }
 
-function nextQuestion() {
-    //if timer is hidden, show it
-    if ($clock.not(':visible')) {
-        $clock.show();
-     }
+        //increment question number to keep track of what question to display
+        questionNum += 1;
 
-    //increment question number to keep track of what question to display
-    questionNum += 1;
+        //clear DOM area
+        $triviaArea.empty();
 
-    //clear DOM area
-    $triviaArea.empty();
-
-    //Show next question
-    createQuestion();
-}
-
-function userGuessed() {
-    var userChoice = $(this).attr("data-choice");
-
-    //Hide timer and stop background processes
-    $clock.hide();
-    disableBackgroundProcesses();
-
-    if (questions[questionNum].correct === userChoice) {
-        //correct guess
-        correctUserGuess();
-    } else {
-        incorrectUserGuess();
-    }
-}
-
-function correctUserGuess() {
-    correctElem = $("<p>").text("Correct!").addClass("correctAnswer");
-
-    //increment number of correct answers
-    correctNum += 1;
-    
-    $triviaArea.html(correctElem);
-
-    //Is there another question
-    if (questionNum < questions.length-1) {
-        //re-enable game
-        setTimeout(enableBackgroundProcesses, secToMs(3));
-        setTimeout(nextQuestion, secToMs(3));
-    } else {
-        setTimeout(gameOver, secToMs(3));
-    }
-}
-
-//user doesn't answer in time
-function timesUp() {
-    //Hide timer
-    $clock.hide();
-
-    incorrectElem = $("<p>").text("Time's Up!").addClass("incorrectAnswer");
-    $("[data-answer='true']").addClass("correctAnswer");
-
-    //Prepend message to DOM
-    $triviaArea.prepend(incorrectElem);
-
-    //Is there another question
-    if (questionNum < questions.length-1) {
-        //re-enable game
-        setTimeout(enableBackgroundProcesses, secToMs(3));
-        setTimeout(nextQuestion, secToMs(3));
-    } else {
-        setTimeout(gameOver, secToMs(3));
-    }
-}
-
-function incorrectUserGuess() {
-    $("[data-answer='true']").addClass("correctAnswer");
-    $("input:checked").closest("label").addClass("incorrectAnswer");
-    incorrectElem = $("<p>").text("Incorrect!").addClass("incorrectAnswer");
-
-    //Prepend message to DOM
-    $triviaArea.prepend(incorrectElem);
-
-    //Is there another question
-    if (questionNum < questions.length-1) {
-        //re-enable game
-        setTimeout(enableBackgroundProcesses, secToMs(3));
-        setTimeout(nextQuestion, secToMs(3));
-    } else {
-        setTimeout(gameOver, secToMs(3));
-    }
-}
-
-function startGame() {
-    //if timer is hidden, show it
-    if ($clock.not(':visible')) {
-        $clock.show();
+        //Show next question
+        createQuestion();
     }
 
-    enableBackgroundProcesses();
+    function userGuessed() {
+        var userChoice = $(this).attr("data-choice");
 
-    //clear DOM area
-    $triviaArea.empty();
+        //Hide timer and stop background processes
+        $clock.hide();
+        disableBackgroundProcesses();
 
-    //hide start button
-    $startBtn.hide();
-    $restartBtn.hide();
+        if (questions[questionNum].correct === userChoice) {
+            //correct guess
+            correctUserGuess();
+        } else {
+            incorrectUserGuess();
+        }
+    }
 
-    //show question area
-    $mainContent.show();
+    function correctUserGuess() {
+        correctElem = $("<p>").text("Correct!").addClass("correctAnswer");
 
-    //set question
-    createQuestion();
-}
+        //increment number of correct answers
+        correctNum += 1;
+        
+        $triviaArea.html(correctElem);
 
-function resetGame() {
-    //reset variables
-    questionNum = 0;
-    correctNum = 0;
+        //Is there another question
+        if (questionNum < questions.length-1) {
+            //re-enable game
+            setTimeout(enableBackgroundProcesses, secToMs(3));
+            setTimeout(nextQuestion, secToMs(3));
+        } else {
+            setTimeout(gameOver, secToMs(3));
+        }
+    }
 
-    startGame();    
-}
+    //user doesn't answer in time
+    function timesUp() {
+        //Hide timer
+        $clock.hide();
 
-//starts interval, resets timer and adds click handler
-function enableBackgroundProcesses() {
-    $(document).on("click", "label", userGuessed);
-    resetTimer();
-    timerInterval = setInterval(reduceTimer, secToMs(1));
-}
+        incorrectElem = $("<p>").text("Time's Up!").addClass("incorrectAnswer");
+        $("[data-answer='true']").addClass("correctAnswer");
 
-//stops interval and removes click handler
-function disableBackgroundProcesses() {
-    $(document).off("click", "label", userGuessed);
-    clearInterval(timerInterval);
-}
+        //Prepend message to DOM
+        $triviaArea.prepend(incorrectElem);
 
-//set click events
-$startBtn.on("click", startGame);
-$restartBtn.on("click", resetGame);
+        //Is there another question
+        if (questionNum < questions.length-1) {
+            //re-enable game
+            setTimeout(enableBackgroundProcesses, secToMs(3));
+            setTimeout(nextQuestion, secToMs(3));
+        } else {
+            setTimeout(gameOver, secToMs(3));
+        }
+    }
+
+    function incorrectUserGuess() {
+        $("[data-answer='true']").addClass("correctAnswer");
+        $("input:checked").closest("label").addClass("incorrectAnswer");
+        incorrectElem = $("<p>").text("Incorrect!").addClass("incorrectAnswer");
+
+        //Prepend message to DOM
+        $triviaArea.prepend(incorrectElem);
+
+        //Is there another question
+        if (questionNum < questions.length-1) {
+            //re-enable game
+            setTimeout(enableBackgroundProcesses, secToMs(3));
+            setTimeout(nextQuestion, secToMs(3));
+        } else {
+            setTimeout(gameOver, secToMs(3));
+        }
+    }
+
+    function startGame() {
+        //if timer is hidden, show it
+        if ($clock.not(':visible')) {
+            $clock.show();
+        }
+
+        enableBackgroundProcesses();
+
+        //clear DOM area
+        $triviaArea.empty();
+
+        //hide start button
+        $startBtn.hide();
+        $restartBtn.hide();
+
+        //show question area
+        $mainContent.show();
+
+        //set question
+        createQuestion();
+    }
+
+    function resetGame() {
+        //reset variables
+        questionNum = 0;
+        correctNum = 0;
+
+        startGame();    
+    }
+
+    //starts interval, resets timer and adds click handler
+    function enableBackgroundProcesses() {
+        $(document).on("click", "label", userGuessed);
+        resetTimer();
+        timerInterval = setInterval(reduceTimer, secToMs(1));
+    }
+
+    //stops interval and removes click handler
+    function disableBackgroundProcesses() {
+        $(document).off("click", "label", userGuessed);
+        clearInterval(timerInterval);
+    }
+
+    //set click events
+    $startBtn.on("click", startGame);
+    $restartBtn.on("click", resetGame);
+});
